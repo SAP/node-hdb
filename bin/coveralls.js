5f89608d25edbd1f0d3d23a218f7e5eb7a253aaa
Holger Koser <holger.koser@sap.com>
2016-03-17 13:32:21 +0100

    fix coveralls script for node 5

--- bin/coveralls.js
+++ bin/coveralls.js
@@ -5,35 +5,22 @@
 var util = require('util');
 var coveralls = require('coveralls');
 
-(function monkeyPatch(request) {
-  if (process.env.HTTPS_PROXY) {
-    var post = request.post;
-    request.post = function (options, cb) {
-      if (typeof options === 'object') {
-        if (options.url === 'https://coveralls.io/api/v1/jobs') {
-          options.proxy = process.env.HTTPS_PROXY;
-        }
-      }
-      post(options, cb);
-    };
-  }
-  if (process.env.TRAVIS_JOB_ID) {
-    process.env.TRAVIS = '1';
-  }
-  if (process.env.SEND_TO_STDOUT) {
-    coveralls.sendToCoveralls = function (obj, cb) {
-      console.log(util.inspect(obj, {
-        colors: true,
-        depth: 8
-      }));
-      setImmediate(function () {
-        cb(null, {
-          statusCode: 200
-        }, 'OK');
-      });
-    };
-  }
-})(require('coveralls/node_modules/request'));
+if (process.env.TRAVIS_JOB_ID) {
+  process.env.TRAVIS = '1';
+}
+if (process.env.SEND_TO_STDOUT) {
+  coveralls.sendToCoveralls = function (obj, cb) {
+    console.log(util.inspect(obj, {
+      colors: true,
+      depth: 8
+    }));
+    setImmediate(function () {
+      cb(null, {
+        statusCode: 200
+      }, 'OK');
+    });
+  };
+}
 
 var input = '';
 process.stdin
