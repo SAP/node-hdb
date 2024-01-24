071ad5dfd6701f7e18e1214252af75c042ca9c9a
Ian McCurdy <idmccurd@gmail.com>
2024-01-24 09:54:35 -0500

    [Test] Fix hanging ResultSetTransform test

--- test/lib.ResultSetTransform.js
+++ test/lib.ResultSetTransform.js
@@ -120,8 +120,8 @@ describe('Lib', function () {
       });
       var chunks = [];
       rst.on('readable', function () {
-        var value = rst.read();
-        if (value !== null) {
+        var value;
+        while(null !== (value = rst.read())) {
           chunks.push(new Buffer([value]));
         }
       });
