e3353a6c0973719c1782a841650243a8c68a87a3
he-is-harry <harryhe_@hotmail.com>
2025-02-07 09:55:13 -0500

    Changed BINTEXT to be treated as NCLOB
    - Updated the NormalizedTypeCode of BINTEXT so that all inputs to BINTEXT are treated as NCLOB's
        - NCLOB input will be validated on HANA so that the buffer input is valid CESU8
    - Added an integration test to check that invalid inputs to BINTEXT will cause an error to be sent back
        - Since the error validation for each argument is done one at a time, and the character validation
    is on the server, the test does take longer than 2 seconds to run.

--- test/acceptance/db.DataType.js
+++ test/acceptance/db.DataType.js
@@ -518,6 +518,21 @@ describe('db', function () {
         }
         async.waterfall([insert, validateDataSql.bind(null, "select * from BINTEXT_TABLE", [3, 53], expected)], done);
       });
+
+      it('should raise input type error', function (done) {
+        // Character conversion validations are done on the server one at a time, so this test will take longer
+        this.timeout(3000);
+        var invalidValues = [Buffer.from('61c7', 'hex'), Buffer.from('c7', 'hex'), Buffer.from('f09f9880f09f9988', 'hex'),
+          Buffer.from('010100000083b2f2ffadfaa3430564e1fc74b64643', 'hex'), false, 123];
+        var invalidTestData = invalidValues.map(function (testValue) {
+          if (Buffer.isBuffer(testValue)) {
+            return {value: testValue, errMessage: "fatal error: invalid CESU-8 encoding for Unicode string"};
+          } else {
+            return {value: testValue, errMessage: "Cannot set parameter at row: 1. Wrong input for LOB type"};
+          }
+        });
+        async.each(invalidTestData, testDataTypeError.bind(null, 'BINTEXT_TABLE'), done);
+      });
     });
   });
 
